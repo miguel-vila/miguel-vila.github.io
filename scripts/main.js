4a464aceeb6b9d6596775167c86d83e366df2f37
CircleCI <circleci@circleci>
2016-06-05 18:29:52 -0500

    Update (2016-06-05 18:29:52 COT) [ci skip]

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,6 @@
 $('.note .note-content').toggleClass('hide');
 
 $('.note .aside-header').click(function(ev) {
-	console.log('clicked')
     var noteContentElement = $(ev.target.parentElement.parentElement).find('.note-content');
-    console.log('noteContentElement = ', noteContentElement);
     noteContentElement.toggleClass('hide');
 });
